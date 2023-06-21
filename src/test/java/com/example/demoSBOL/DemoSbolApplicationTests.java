4eb338fd49e5b113e90e217cd38d1d774ba29819
moozi3 <120219392+moozi3@users.noreply.github.com>
2023-06-21 14:07:14 -0400

    Add files via upload

---
+++ src/test/java/com/example/demoSBOL/DemoSbolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoSBOL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSbolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
