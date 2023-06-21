32c62355c23fac1bcc861b14fa82f664ea99f639
terradistac <terradistacc@gmail.com>
2023-06-21 15:20:03 -0400

    Now runs using Spring Boot

--- src/main/java/com/example/demoSBOL/HelloController.java
+++ src/main/java/com/example/demoSBOL/HelloController.java
@@ -1,13 +1,13 @@
 package com.example.demoSBOL;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
+	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot";
+		return "Greetings from Spring Boot!";
 	}
 }
